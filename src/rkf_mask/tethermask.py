1f39a9deedcabf6b94c5a968d4fd3bca0e5f796c
Kellan Moorse <kmoorse@caltech.edu>
2019-01-24 16:12:45 -0800

    adding diagnostic draw option

--- src/rkf_mask/tethermask.py
+++ src/rkf_mask/tethermask.py
@@ -14,7 +14,6 @@ class TetherMask(object):
         sub_topic = rospy.get_param(rospy.resolve_name("~input_image"), "stabilized_image")
         pub_topic = rospy.get_param(rospy.resolve_name("~output_image"), "masked_image")
 
-        self.trunc = rospy.get_param(rospy.resolve_name("~truncation"), 0.5)
         self.img_pub = rospy.Publisher(pub_topic, Image, queue_size=10)
         self.img_sub = rospy.Subscriber(sub_topic, Image, self.callback)
         self.bridge = CvBridge()
@@ -41,7 +40,7 @@ class TetherMask(object):
 
         # Find tether polygon
         # ***Assumes tether edges are the longest straight lines in the ROI***
-        lsd = cv2.createLineSegmentDetector()
+        lsd = cv2.createLineSegmentDetector(_refine=cv2.LSD_REFINE_NONE)
         ly, lx = img.shape
         # ROI is the top-middle segment of a 2x3 grid
         lines = lsd.detect(img[:int(ly / 2), int(lx / 3):int(2 * lx / 3)])
@@ -51,16 +50,12 @@ class TetherMask(object):
         points[:, 0] += lx / 3
 
         # Truncate tether polygon
+        trunc = rospy.get_param(rospy.resolve_name("~truncation"), 0.5)
         for i in range(2):
             sign = np.sign(points[2*i, 1] - points[2*i+1, 1])
             top = 2*i + int(sign+1)/2
             bot = 2*i + int(-sign+1)/2
-            points[top, :] += self.trunc * (points[bot, :] - points[top, :])
-
-        # Extract region unaffected by stabilization border artifacts
-        nzx = sum(img[-1, :] != 0)
-        nzy = sum(img[:, 1] != 0)
-        img_tmp = img[-nzy:, :nzx]
+            points[top, :] += trunc * (points[bot, :] - points[top, :])
 
         mask = img * 0
         cv2.fillPoly(mask, [points.astype('int32')], 255)
@@ -68,6 +63,11 @@ class TetherMask(object):
 
         # Inpaint over tether mask
         img = cv2.inpaint(img, mask, 10, cv2.INPAINT_TELEA)
+
+        if rospy.get_param(rospy.resolve_name("~draw_diagnostic"), False):
+            for line in lines[0]:
+                x1, y1, x2, y2 = line[0]
+                cv2.line(img, (int(x1+lx/3), y1), (int(x2+lx/3), y2), (255, 255, 255))
 
         return img
 
